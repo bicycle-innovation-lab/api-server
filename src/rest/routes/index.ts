4aa9082cf2bc5e0bc255b9fd6154252a548368ba
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-04-09 10:45:53 +0200

    Add token issue endpoint

--- src/rest/routes/index.ts
+++ src/rest/routes/index.ts
@@ -1,8 +1,10 @@
 import * as Router from "koa-router";
 import UsersRouter from "./users";
+import TokensRouter from "./tokens";
 
 const Routes = new Router();
 
 Routes.use("/users", UsersRouter.routes(), UsersRouter.allowedMethods());
+Routes.use("/tokens", TokensRouter.routes(), TokensRouter.allowedMethods());
 
 export default Routes;
---
+++ src/rest/routes/tokens.ts
@@ -0,0 +1,30 @@
+import * as Router from "koa-router";
+import * as Joi from "joi";
+import {UserModel} from "../../db/user";
+
+const TokensRequestSchema = Joi.object({
+    email: Joi.string().required(),
+    password: Joi.string().required()
+});
+
+const TokensRouter = new Router();
+
+TokensRouter.post("/", async (ctx, next) => {
+    const body = await ctx.request.json();
+
+    const {error, value} = Joi.validate(body, TokensRequestSchema);
+    if (error) {
+        ctx.throw(400, error.details[0].message)
+    }
+
+    const {email, password} = value;
+    const user = await UserModel.findOne({email});
+
+    if (!user || !(await user.comparePassword(password))) {
+        ctx.throw(400, "Wrong email or password");
+    } else {
+        return await user.issueToken();
+    }
+});
+
+export default TokensRouter;
