b87dbc1786242e67014a6fd92ed47e73bf8824ca
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-04-09 09:47:36 +0200

    Add koa-body-parsers

--- src/rest/server.ts
+++ src/rest/server.ts
@@ -1,11 +1,14 @@
 import * as Koa from "koa";
 import * as KoaJWT from "koa-jwt";
+import * as KoaBodyParser from "koa-body-parsers";
 import {Secret} from "../auth/token";
 import Routes from "./routes";
 import PopulateUser from "./middleware/populate-user";
 import FormatResponse from "./middleware/format-message";
 
 export const Server = new Koa();
+
+KoaBodyParser(Server);
 
 Server.use(KoaJWT({secret: Secret, passthrough: true, key: "userId"}));
 Server.use(PopulateUser);
---
+++ src/types/koa-body-parsers/index.d.ts
@@ -0,0 +1,4 @@
+import * as Koa from "koa";
+
+declare const BodyParsers: (app: Koa) => void;
+export = BodyParsers;
