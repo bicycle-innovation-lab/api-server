92ad1a054fc930d9aea34cfdd7ebc425403d531a
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-04-09 11:35:49 +0200

    Move permission logic into testPermission

--- src/rest/middleware/require-permissions.ts
+++ src/rest/middleware/require-permissions.ts
@@ -1,20 +1,29 @@
 import * as Koa from "koa";
 import {AuthLevel, getRoleLevel} from "../../auth/role";
 
-const RequirePermission: (level: AuthLevel) => Koa.Middleware = level => async (ctx, next) => {
-    const user = await ctx.state.getUser();
+/**
+ * Only returns when the currently signed in user has sufficient permissions. Throws the appropriate error when the
+ * request unauthenticated, or has insufficient permissions.
+ */
+export async function testPermission(context: Koa.Context, level: AuthLevel) {
+    const user = await context.state.getUser();
     if (user) {
         const userLevel = getRoleLevel(user.role);
         if (userLevel >= level) {
-            return await next();
+            return true
         } else {
             // user does not have high enough auth level
-            ctx.throw(403, "User has insufficient permissions");
+            context.throw(403, "User has insufficient permissions");
         }
     } else {
         // no user is signed in
-        ctx.set("WWW-Authenticate", "Bearer");
-        ctx.throw(401, "Not authenticated");
+        context.set("WWW-Authenticate", "Bearer");
+        context.throw(401, "Not authenticated");
     }
+}
+
+const RequirePermission: (level: AuthLevel) => Koa.Middleware = level => async (ctx, next) => {
+    await testPermission(ctx, level);
+    return await next();
 };
 export default RequirePermission;
