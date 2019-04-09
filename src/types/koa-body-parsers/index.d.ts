a5d571534651f0264f829b75dda520d4d4895d8e
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-04-09 09:53:59 +0200

    Update koa-body-parsers definitions

--- src/types/koa-body-parsers/index.d.ts
+++ src/types/koa-body-parsers/index.d.ts
@@ -2,3 +2,9 @@ import * as Koa from "koa";
 
 declare const BodyParsers: (app: Koa) => void;
 export = BodyParsers;
+
+declare module "koa" {
+    interface BaseRequest {
+        json(): Promise<any>;
+    }
+}
