e3a43e852fabfe4b8ecc9a00547328e054db7d13
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-04-09 09:32:18 +0200

    Properly format response data and errors

---
+++ src/rest/middleware/format-message.ts
@@ -0,0 +1,22 @@
+import * as Koa from "koa";
+
+const FormatMessage: Koa.Middleware = async (ctx, next) => {
+    try {
+        const data = await next();
+        ctx.body = JSON.stringify({data, code: 200});
+    } catch (err) {
+        if ((err.status || 500) >= 500) {
+            console.error("Internal error", err);
+        }
+        const res = {
+            code: err.status || 500,
+            error: err.expose === true ?
+                err.message :
+                err.name || "Unknown Error"
+        };
+        ctx.body = JSON.stringify(res);
+    } finally {
+        ctx.type = "json";
+    }
+};
+export default FormatMessage;
--- src/rest/server.ts
+++ src/rest/server.ts
@@ -3,17 +3,14 @@ import * as KoaJWT from "koa-jwt";
 import {Secret} from "../auth/token";
 import Routes from "./routes";
 import PopulateUser from "./middleware/populate-user";
+import FormatResponse from "./middleware/format-message";
 
 export const Server = new Koa();
 
 Server.use(KoaJWT({secret: Secret, passthrough: true, key: "userId"}));
 Server.use(PopulateUser);
 
-// reformat data before sending
-Server.use(async (ctx, next) => {
-    const data = await next();
-    ctx.body = JSON.stringify({data, code: 200});
-});
+Server.use(FormatResponse);
 
 Server.use(Routes.routes());
 Server.use(Routes.allowedMethods());
