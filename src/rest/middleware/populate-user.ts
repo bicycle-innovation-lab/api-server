943afebe4dd9f9bff9cc31bfde6edb4c56530b89
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-04-10 12:28:24 +0200

    Added ctx.state.authenticated

--- src/rest/middleware/populate-user.ts
+++ src/rest/middleware/populate-user.ts
@@ -6,9 +6,10 @@ const PopulateUser: Koa.Middleware = async (ctx, next) => {
     if (ctx.state.authToken) {
         ctx.state.authType = ctx.state.authToken.type as TokenType;
     }
+    ctx.state.authenticated = !!ctx.state.authToken;
     ctx.state.getUser = async () => {
         // fetch user data if the user is signed in, and the user data has not yet been fetched from the db
-        if (!ctx.state.user && ctx.state.authToken) {
+        if (!ctx.state.user && ctx.state.authenticated) {
             ctx.state.user = await UserModel.findById(ctx.state.authToken.sub);
         }
         return ctx.state.user as User | undefined;
