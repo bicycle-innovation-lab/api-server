9a192e81bd7a890ae76ea6dcd20c195ec556a845
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-04-10 14:21:53 +0200

    Require token schema

--- src/rest/schema/tokens.ts
+++ src/rest/schema/tokens.ts
@@ -3,8 +3,8 @@ import {joi, Email, Password} from "./common";
 export const SessionTokenRequestSchema = joi.object({
     email: Email().required(),
     password: Password().required()
-});
+}).required();
 
 export const ResetPasswordTokenRequestSchema = joi.object({
     email: Email().required()
-});
+}).required();
