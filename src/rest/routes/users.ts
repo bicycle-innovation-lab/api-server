2443214f9c36b726d3c9ee75f56bb2bd8784640a
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-04-10 13:46:27 +0200

    Add canSeeUser utility function

--- src/rest/routes/users.ts
+++ src/rest/routes/users.ts
@@ -1,6 +1,21 @@
+import * as Koa from "koa";
 import * as Router from "koa-router";
 import PostUsers from "./users.post";
 import GetUsers from "./users.get";
+import {AuthLevel} from "../../auth/role";
+
+export async function canSeeUser(id: string, ctx: Koa.BaseContext): Promise<boolean> {
+    const signedIn = await ctx.state.getUser();
+    if (!signedIn) {
+        return false;
+    }
+    if (id !== "me") {
+        if (signedIn.authLevel < AuthLevel.Manager) {
+            return false;
+        }
+    }
+    return true;
+}
 
 const UsersRouter = new Router();
 
