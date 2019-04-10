cd8294567df617a5c7e212d3b217cf7cf89bed49
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-04-10 11:39:52 +0200

    Fix populate-user

--- src/rest/middleware/populate-user.ts
+++ src/rest/middleware/populate-user.ts
@@ -3,11 +3,13 @@ import {User, UserModel} from "../../db/user";
 import {TokenType} from "../../auth/token";
 
 const PopulateUser: Koa.Middleware = async (ctx, next) => {
+    if (ctx.state.authToken) {
+        ctx.state.authType = ctx.state.authToken.type as TokenType;
+    }
     ctx.state.getUser = async () => {
         // fetch user data if the user is signed in, and the user data has not yet been fetched from the db
         if (!ctx.state.user && ctx.state.authToken) {
-            ctx.state.authType = ctx.state.authToken.type as TokenType;
-            ctx.state.user = await UserModel.findById(ctx.state.userToken.sub);
+            ctx.state.user = await UserModel.findById(ctx.state.authToken.sub);
         }
         return ctx.state.user as User | undefined;
     };
