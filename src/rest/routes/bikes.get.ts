5c7d10eaaaa362068bbee6479bae1430e3f96a86
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-04-11 11:32:23 +0200

    Add GET /bikes/id

--- src/rest/routes/bikes.get.ts
+++ src/rest/routes/bikes.get.ts
@@ -4,3 +4,13 @@ import {BikeModel} from "../../db/bike";
 export const GetMultipleBikes: Koa.Middleware = async () => {
     return (await BikeModel.find()).map(it => it.toCleanObject());
 };
+
+export const GetOneBike: Koa.Middleware = async ctx => {
+    const {id} = ctx.params;
+    const bike = await BikeModel.findById(id);
+    if (!bike) {
+        ctx.throw(404);
+    } else {
+        return bike;
+    }
+};
--- src/rest/routes/bikes.ts
+++ src/rest/routes/bikes.ts
@@ -1,10 +1,11 @@
 import * as Router from "koa-router";
-import {GetMultipleBikes} from "./bikes.get";
+import {GetMultipleBikes, GetOneBike} from "./bikes.get";
 import PostBikes from "./bikes.post";
 
 const BikesRouter = new Router();
 
 BikesRouter.post("/", PostBikes);
 BikesRouter.get("/", GetMultipleBikes);
+BikesRouter.get("/:id", GetOneBike);
 
 export default BikesRouter;
