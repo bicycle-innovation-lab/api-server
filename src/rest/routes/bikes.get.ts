6affd2434899cfa738e421c8fbae207bba3dbd5b
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-04-11 11:36:50 +0200

    Check if id is valid ObjectID

--- src/rest/routes/bikes.get.ts
+++ src/rest/routes/bikes.get.ts
@@ -1,5 +1,6 @@
 import * as Koa from "koa";
 import {BikeModel} from "../../db/bike";
+import {ObjectId} from "../schema/common";
 
 export const GetMultipleBikes: Koa.Middleware = async () => {
     return (await BikeModel.find()).map(it => it.toCleanObject());
@@ -7,6 +8,9 @@ export const GetMultipleBikes: Koa.Middleware = async () => {
 
 export const GetOneBike: Koa.Middleware = async ctx => {
     const {id} = ctx.params;
+    if (ObjectId().validate(id).error) {
+        ctx.throw(404);
+    }
     const bike = await BikeModel.findById(id);
     if (!bike) {
         ctx.throw(404);
