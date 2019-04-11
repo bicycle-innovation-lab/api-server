ab2116d8203b973c28cc244813edd1d56831e402
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-04-11 11:34:05 +0200

    Add type to joi

--- src/rest/schema/common.ts
+++ src/rest/schema/common.ts
@@ -2,7 +2,7 @@ import * as Joi from "joi";
 import * as JoiPhoneNumber from "joi-phone-number";
 import {allRoles} from "../../auth/role";
 
-export const joi = Joi.extend(JoiPhoneNumber);
+export const joi: typeof Joi = Joi.extend(JoiPhoneNumber);
 
 export const Password = () => joi.string().max(72);
 export const Email = () => joi.string().email();
