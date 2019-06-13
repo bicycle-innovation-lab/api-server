e138f8a1b8b31c6b5ff888c0cbe5057e43b74a2f
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-06-13 09:08:45 +0200

    Add GET /bookings

--- src/rest/routes/booking/index.ts
+++ src/rest/routes/booking/index.ts
@@ -1,10 +1,13 @@
 import * as Router from "koa-router";
 // import {GetMultipleBikes, GetOneBike} from "./bikes.get";
 import PostBookings from "./bookings.post";
+import {GetMultipleBookings, GetOneBooking} from "./bookings.get";
 
 const BookingsRouter = new Router();
 
 BookingsRouter.post("/", PostBookings);
+BookingsRouter.get("/", GetMultipleBookings);
+BookingsRouter.get("/:id", GetOneBooking);
 // BookingsRouter.get("/", GetMultipleBookings);
 // BookingsRouter.get("/:id", GetOneBooking);
 
