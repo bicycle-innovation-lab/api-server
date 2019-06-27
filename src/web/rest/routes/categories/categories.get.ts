5ee1efc92e74b3bc05c619beeda00dcd2ba4a9c6
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-06-27 14:17:54 +0200

    Add endpoint to get bikes belonging to a category

--- src/web/rest/routes/categories/categories.get.ts
+++ src/web/rest/routes/categories/categories.get.ts
@@ -1,5 +1,6 @@
 import * as Koa from "koa";
 import * as Logic from "../../../logic/categories";
+import * as BikeLogic from "../../../logic/bikes";
 
 export const GetMultipleCategories: Koa.Middleware = async ctx => {
     const categories = await Logic.listCategories(ctx);
@@ -15,4 +16,15 @@ export const GetOneCategory: Koa.Middleware = async ctx => {
 
     ctx.status = 200;
     return cat;
+};
+export const GetCategoryBikes: Koa.Middleware = async ctx => {
+    const {id} = ctx.params;
+    const cat = await Logic.getCategory(ctx, id);
+    if (!cat) {
+        return ctx.throw(404);
+    }
+
+    const bikes = await BikeLogic.listBikes(ctx, {categories: cat.id});
+    ctx.status = 200;
+    return bikes;
 };
