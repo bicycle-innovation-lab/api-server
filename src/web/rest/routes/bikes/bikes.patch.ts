a4af0ecf8aca5d1523a81ad2ddbaa0a956a39e29
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-08-20 11:26:52 +0200

    Add PATCH /bikes/id route handler

---
+++ src/web/rest/routes/bikes/bikes.patch.ts
@@ -0,0 +1,29 @@
+import * as Koa from "koa";
+import * as compose from "koa-compose";
+import RequirePermission from "../../../middleware/require-permissions";
+import {AuthLevel} from "../../../../auth/role";
+import {PatchBikeRequestSchema} from "../../schema/bikes";
+import * as Logic from "../../../logic/bikes";
+import {ObjectId} from "../../../schema/common";
+
+const PatchBike: Koa.Middleware = compose([
+    RequirePermission(AuthLevel.Manager),
+    async ctx => {
+        const {id} = ctx.params;
+        if (ObjectId().validate(id).error) {
+            return ctx.throw(404);
+        }
+
+        const form = await ctx.validateBody(PatchBikeRequestSchema);
+        const success = await Logic.updateBike(ctx, id, form);
+
+        if (success) {
+            ctx.status = 200;
+        } else {
+            ctx.status = 404;
+        }
+        return {success};
+    }
+]);
+
+export default PatchBike;
--- src/web/rest/routes/bikes/index.ts
+++ src/web/rest/routes/bikes/index.ts
@@ -1,10 +1,12 @@
 import * as Router from "koa-router";
 import {GetBikeBookings, GetMultipleBikes, GetOneBike} from "./bikes.get";
 import PostBikes from "./bikes.post";
+import PatchBike from "./bikes.patch";
 
 const BikesRouter = new Router();
 
 BikesRouter.post("/", PostBikes);
+BikesRouter.patch("/:id", PatchBike);
 BikesRouter.get("/", GetMultipleBikes);
 BikesRouter.get("/:id", GetOneBike);
 BikesRouter.get("/:id/bookings", GetBikeBookings);
