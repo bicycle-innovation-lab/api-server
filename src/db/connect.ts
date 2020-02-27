5e00c80cc5c142b5e177fec1bc0fb56e13834996
klementj <klementj@gmail.com>
2020-02-27 22:46:09 +0100

    :card_file_box: Add new mongoose options
    Adding new options to remove moongose deprecation warnings

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -3,5 +3,9 @@ import * as Mongoose from "mongoose";
 const MongoDBUrl = process.env.MONGODB_URL || "mongodb://localhost:27017/admin";
 
 export default async function Connect() {
-    await Mongoose.connect(MongoDBUrl, {useNewUrlParser: true});
+    await Mongoose.connect(MongoDBUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    });
 }
